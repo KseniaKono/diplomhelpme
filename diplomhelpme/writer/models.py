c458e220a2a995a8b091d218d76cc9b34b08a5c4
KseniaKono <100347521+KseniaKono@users.noreply.github.com>
2023-04-17 22:58:27 +0300

    homepage

--- diplomhelpme/writer/models.py
+++ diplomhelpme/writer/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+import uuid
+
+from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+import uuid
+
+from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+
+class Content(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Уникальный id для каждого произведения')
+    author = models.ForeignKey(User, verbose_name='Автор', on_delete=models.CASCADE)
+    ganre = models.ForeignKey('ContentType', on_delete=models.SET_NULL, null=True, verbose_name='Жанр')
+    name = models.CharField('Название', max_length=200)
+    description = models.TextField('Аннотация', max_length=500, null=True)
+    data = models.TextField('Текст')
+
+    def __str__(self):
+        return '%s (%s)' % (self.name, str(self.id))
+
+    def get_absolute_url(self):
+        return reverse('content-detail', args=[str(self.id)])
+
+
+
+class ContentType(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Уникальный id для каждого жанра')
+    name = models.CharField('Название', max_length=150)
+    description = models.TextField('Описание', max_length=1000, blank=True, null=True)
+
+    def __str__(self):
+        return '%s (%s)' % (self.name, str(self.id))
--- diplomhelpme/writer/views.py
+++ diplomhelpme/writer/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from .models import Content, ContentType
+from django.contrib.auth.models import User
 # Create your views here.
+
+
+def index(request):
+    """
+    Функция отображения для домашней страницы сайта.
+    """
+    # Генерация "количеств" некоторых главных объектов
+    num_Content=Content.objects.all().count()
+    num_Users=User.objects.all().count()
+
+
+    # Отрисовка HTML-шаблона index.html с данными внутри
+    # переменной контекста context
+    return render(
+        request,
+        'index.html',
+        context={'num_Content':num_Content,'num_Users':num_Users},
+    )
